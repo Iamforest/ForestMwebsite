3cb78e80c4c4e76807983f8576f42f70161d6531
Forest McDonald <forest383@gmail.com>
2023-05-30 13:47:35 -0400

    Filename changes and added redirect to homepage

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
+@app.route("/")
+def redirectToHome():
+    return redirect("/home")
+
 @app.route("/home")
 def home():
-    return render_template("menuTemplate.html")
+    return render_template("index.html")
 
 @app.route("/projects")
 def projects():
