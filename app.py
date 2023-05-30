533d6da5e4f5e442fa9bc63003262b643135e255
Iamforest <forest383@gmail.com>
2023-05-30 11:07:24 -0400

    Added pages, requirements, made pretty

--- app.py
+++ app.py
@@ -3,13 +3,17 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/home")
 def home():
     return render_template("menuTemplate.html")
 
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
 
-
-
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 
 if __name__ == '__main__':
